0d43114d361254ad4d5909aaf53361d7d6ffdd1d
trohee <roheethomas@gmail.com>
2013-08-17 14:23:44 -0400

    add listener

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -8,14 +8,27 @@ chrome.runtime.onMessage.addListener(
 		if(request.msg === "usergin"){
 			socket.emit("usergin", {username: request.form});
 
-			socket.on("", function(data){
-
+			socket.on("userExist", function(data){
+				if(data.result){
+					senderResponse({success: true, users: data.users});
+				} else{
+					senderResponse({success: false});
+				}
+				
 			});
-			
-			senderResponse({success: true});
+
 		} else if (request.msg === "question"){
 			socket.emit("usergin", {question: request.form.question, answer: request.form.answer});
-			senderResponse({success: true});
-		}
+
+			socket.on("", function(data){
+				senderResponse({success: true});
+			});
+		} else if (request.msg === "userList"){
+			socket.emit("usergin", {question: request.form.question, answer: request.form.answer});
+
+			socket.on("", function(data){
+				senderResponse({success: true});
+			});
+		} 
 	}
 );
