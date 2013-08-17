21f45f6cb8452babc4d60f4b6ec7b75ed76ac957
jrobertou <jeremy.robertou@gmail.com>
2013-08-17 11:38:48 -0400

    background communication beginning

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,13 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+	console.log('previousVersion', details.previousVersion);
 });
 
-
+chrome.runtime.onMessage.addListener(
+	function(request,sender,senderResponse){
+		if(request.msg==="form"){
+			console.log("receive from socket server: "+JSON.stringify(form));
+		}
+	}
+);
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,25 +1,13 @@
 'use strict';
 
-var triviasnap =  {
-	formListener = function(){
-		$('form').submit(function(event) {
-			event.preventDefault();
-			$.ajax({
-				type: $(this).attr('method'),
-				url: $(this).attr('action'),
-				data: $(this).serialize(), 
-				dataType: 'json',
-				success: function(data) {
-			    if(data.success) {
-			    }
-			    else {
-			    }
-				},
-				error: function(data) {
-				  alert('Une erreur est survenue.');
-				} 
-			});
-			return false;
+document.addEventListener('DOMContentLoaded', function () {
+	$('form').submit(function(e) {
+		e.preventDefault();
+		chrome.runtime.sendMessage({msg: $(this).attr('action'), form:$(this).serialize()},function(response){
+			if(response.success) {
+
+		  }
 		});
-	}
-};
+		return false;
+	});
+});
